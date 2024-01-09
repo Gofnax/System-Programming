28b592c63d640bc71d8569a2b0ba371a1df223fc
Emil <37872383+Gofnax@users.noreply.github.com>
2024-01-09 15:30:54 +0000

    Add code to more files

--- Lab2/exe.c
+++ Lab2/exe.c
@@ -28,9 +28,17 @@ void q1_B()
 
 }
 
+#define ROWS 4
 void q2()
 {
+    char mat[ROWS][COLS];
+    initMat(mat, ROWS, 3);
+    printMat(mat, ROWS, 3);
 
+    char tav = 'B';
+    int c = countCharInMat(mat, ROWS, 3, tav);
 	
+    if(c != -1)
+        printf("%c is %d in matrix\n", tav, c);
 
 }
--- Lab2/lab2.c
+++ Lab2/lab2.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <ctype.h>
 #include "lab2.h"
 
 int checkArray(int arr[], int size)
@@ -9,8 +11,25 @@ int checkArray(int arr[], int size)
     return 1;
 }
 
-/*
 int countCharInMat(char mat[][COLS], int rows, int cols, char tav)
 {
+    if(cols > COLS)
+    {
+        printf("Error\n");
+        return -1;
+    }
 
-}*/
+    int count = 0;
+    char checkTav = tolower(tav);
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < cols; j++)
+        {
+            if(checkTav == tolower(mat[i][j]))
+                count++;
+        }
+    }
+
+    return count;
+}
