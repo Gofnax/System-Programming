b56b003995fef4400a2f5d1260b06b5df784780c
Emil <37872383+Gofnax@users.noreply.github.com>
2024-02-01 14:52:54 +0000

    Continue pictureLine question

--- H1_Publish/pictureLines.c
+++ H1_Publish/pictureLines.c
@@ -2,10 +2,11 @@
 
 void addLines()
 {
-    //int mat[ROWS][COLS] = { 0 };
+    int mat[ROWS][COLS] = { 0 };
     int rows = 0, cols = 0;
     printMatDimensions(ROWS, COLS);
     getEffectiveMatDimensions(&rows, &cols, ROWS, COLS);
+    linesInput((int*)mat, rows, cols);
 }
 
 void printMatDimensions(int rows, int cols)
@@ -30,3 +31,73 @@ void getEffectiveMatDimensions(int* rows, int* cols, int maxRows, int maxCols)
     printf("Effective size %d*%d\n", *rows, *cols);
 }
 
+void linesInput(int* mat, int rows, int cols)
+{
+    int option = 1, isLegalLine;
+    int x1, y1, x2, y2;
+
+    while(option)
+    {
+        isLegalLine = getLine(rows, cols, &x1, &y1, &x2, &y2);
+        if(!isLegalLine)
+        {
+            repeatLegalInput(rows, cols, &x1, &y1, &x2, &y2);
+        }
+        printf("Do you want to insert another line? 1 - yes, 0 - no:\n");
+        scanf("%d", &option);
+    }
+
+    if(checkLineValid(mat, rows, cols, x1, y1, x2, y2))
+    {
+        
+    }
+}
+
+void repeatLegalInput(int rows, int cols, int* pX1, int* pY1, int* pX2, int* pY2)
+{
+    int isLegalLine;
+    do
+    {
+        printf("Illegal line. Insert again\n");
+        isLegalLine = getLine(rows, cols, pX1, pY1, pX2, pY2);
+    } while (!isLegalLine);
+}
+
+void inputCoodrdination(int* num)
+{
+    int buff;
+    scanf("%d", &num);
+    scanf("%d", &buff);
+}
+
+int getLine(int rows, int cols, int* pX1, int* pY1, int* pX2, int* pY2)
+{
+    printf("Insert line parameters x1, y1, x2, y2");
+    //inputCoodrdination(pX1);
+    //inputCoodrdination(pY1);
+    //inputCoodrdination(pX2);
+    //inputCoodrdination(pY2);
+    scanf("%d", pX1);
+    scanf("%d", pY1);
+    scanf("%d", pX2);
+    scanf("%d", pY2);
+    if(*pX1 >= 0 && *pX1 < rows)
+    {
+        if(*pY1 >= 0 && *pY1 < cols)
+        {
+            if(*pX2 >= 0 && *pX2 < rows)
+            {
+                if(*pY2 >= 0 && *pY2 < cols)
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int checkLineValid(int* mat, int rows, int cols, int x1, int y1, int x2, int y2)
+{
+    
+}
--- H1_Publish/pictureLines.h
+++ H1_Publish/pictureLines.h
@@ -9,5 +9,9 @@
 void addLines();
 void printMatDimensions(int rows, int cols);
 void getEffectiveMatDimensions(int* rows, int* cols, int maxRows, int maxCols);
+void linesInput(int rows, int cols);
+void inputCoodrdination(int* num);
+int getLine(int rows, int cols, int* pX1, int* pY1, int* pX2, int* pY2);
+
 
 #endif
