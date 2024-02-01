860252985261132cfec76f25057580900ce6f478
Emil <37872383+Gofnax@users.noreply.github.com>
2024-02-01 17:17:31 +0000

    Possibly finish pictureLines

--- H1_Publish/pictureLines.c
+++ H1_Publish/pictureLines.c
@@ -4,9 +4,11 @@ void addLines()
 {
     int mat[ROWS][COLS] = { 0 };
     int rows = 0, cols = 0;
+    int numOfLines = 0;
     printMatDimensions(ROWS, COLS);
     getEffectiveMatDimensions(&rows, &cols, ROWS, COLS);
-    linesInput((int*)mat, rows, cols, COLS);
+    numOfLines = linesInput((int*)mat, rows, cols, COLS);
+    printf("There are %d lines in the matrix.\n", numOfLines);
 }
 
 void printMatDimensions(int rows, int cols)
@@ -31,11 +33,11 @@ void getEffectiveMatDimensions(int* rows, int* cols, int maxRows, int maxCols)
     printf("Effective size %d*%d\n", *rows, *cols);
 }
 
-void linesInput(int* mat, int rows, int cols, int maxCols)
+int linesInput(int* mat, int rows, int cols, int maxCols)
 {
     int option = 1, isLegalLine;
     int x1, y1, x2, y2;
-
+    int numOfLines = 0;
     while(option)
     {
         isLegalLine = getLine(rows, cols, &x1, &y1, &x2, &y2);
@@ -43,14 +45,21 @@ void linesInput(int* mat, int rows, int cols, int maxCols)
         {
             repeatLegalInput(rows, cols, &x1, &y1, &x2, &y2);
         }
+
+        if(checkLineValid(mat, rows, cols, maxCols, x1, y1, x2, y2))
+        {
+            insertLineToMat(mat, maxCols, x1, y1, x2, y2);
+            numOfLines++;
+        }
+        else
+        {
+            printf("The line can't be entered\n");
+        }
+        
         printf("Do you want to insert another line? 1 - yes, 0 - no:\n");
         scanf("%d", &option);
     }
-
-    if(checkLineValid(mat, rows, cols, maxCols, x1, y1, x2, y2))
-    {
-        
-    }
+    return numOfLines;
 }
 
 void repeatLegalInput(int rows, int cols, int* pX1, int* pY1, int* pX2, int* pY2)
@@ -72,11 +81,7 @@ void inputCoodrdination(int* num)
 
 int getLine(int rows, int cols, int* pX1, int* pY1, int* pX2, int* pY2)
 {
-    printf("Insert line parameters x1, y1, x2, y2");
-    //inputCoodrdination(pX1);
-    //inputCoodrdination(pY1);
-    //inputCoodrdination(pX2);
-    //inputCoodrdination(pY2);
+    printf("Insert line parameters x1, y1, x2, y2\n");
     scanf("%d", pX1);
     scanf("%d", pY1);
     scanf("%d", pX2);
@@ -118,7 +123,7 @@ int checkLineValid(int* mat, int rows, int cols, int maxCols,  int x1, int y1, i
         if(y2 != rows - 1 && x2 != cols - 1 && *(mat + (y2 + 1) * maxCols + (x2 + 1)) == 1)    //checks point right and below
             return 0;
     }
-    else if(y1 == y2)
+    else if(y1 == y2)   //if horizontal line
     {
         if(y1 != 0 && x1 != 0 && *(mat + (y1 - 1) * maxCols) + (x1 - 1) == 1)   //checks point left and above
             return 0;
@@ -138,7 +143,7 @@ int checkAdjacentRows(int* mat, int rows, int maxCols, int x1, int y1, int x2, i
     {
         if(y1 != 0 && *(mat + (y1 - 1) * maxCols + i) == 1) //checks the row above the line
             return 0;
-        if(y1 != rows - 1 && *(mat + (y1 + 1) * maxCols + x1 + i) == 1)  //checks the row below the line
+        if(y1 != rows - 1 && *(mat + (y2 + 1) * maxCols + x1 + i) == 1)  //checks the row below the line
             return 0;
     }
     return 1;
@@ -150,7 +155,26 @@ int checkAdjacentCols(int* mat, int cols, int maxCols, int x1, int y1, int x2, i
     {
         if(x1 != 0 && *(mat + (y1 + i) * maxCols + x1 - 1) == 1)    //checks the column to the left of the line
             return 0;
-        if(x1 != cols - 1 && *(mat + (y1 + i) * maxCols + x1 + 1) == 1) //check the column to the right of the line
+        if(x1 != cols - 1 && *(mat + (y1 + i) * maxCols + x2 + 1) == 1) //check the column to the right of the line
             return 0;
+    }
+    return 1;
+}
+
+void insertLineToMat(int* mat, int maxCols, int x1, int y1, int x2, int y2)
+{
+    if(x1 == x2)    //if vertical line
+    {
+        for(int i = y1; i <= y2; i++)
+        {
+            *(mat + i * maxCols + x1) = 1;
+        }
+    }
+    else    //if horizontal line or single point
+    {
+        for(int i = x1; i <= x2; i++)
+        {
+            *(mat + y1 * maxCols + i) = 1;
+        }
     }
 }
