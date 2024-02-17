921df53e3e1cf96614c96b8d6bce3ac317c198bd
Gofnax <glater.emil@gmail.com>
2024-02-17 20:17:49 +0200

    Add functions to 'airline.c' and 'airline.h'

--- HW2/airline.c
+++ HW2/airline.c
@@ -1,2 +1,71 @@
 #include "airline.h"
 
+int addFlight(Flight* pFlight, Airline* pAirline)
+{
+    if(pAirline == NULL)
+    {
+        return 0;
+    }
+    if(doesFlightExist(pFlight, pAirline->flightArr, pAirline->flightCount))
+    {
+        return 0;
+    }
+    pAirline->flightArr[pAirline->flightCount - 1] = pFlight;
+    pAirline->flightCount++;
+    return 1;
+}
+
+int addPlane(Plane* pPlane, Airline* pAirline)
+{
+    if(pAirline == NULL)
+    {
+        return 0;
+    }
+    if(doesPlaneExist(pPlane, pAirline->planeArr, pAirline->planeCount))
+    {
+        return 0;
+    }
+    pAirline->planeArr[pAirline->planeCount] = *pPlane;
+    pAirline->flightCount++;
+    return 1;
+}
+
+void doPrintFlightsWithPlaneType(void)
+{
+    int input;
+    printf("Enter a plane type: (0 - Commercial, 1 - Cargo, 2 - Military)\n");
+    (void)scanf("%d", &input);
+    //needs to be updated when 'Flight.c' will be integrated
+}
+
+int doesFlightExist(Flight* pFlight, Flight** flightArr, size_t flightCount)
+{
+    if(flightArr == NULL)
+    {
+        return 0;
+    }
+    for(size_t i = 0; i < flightCount; i++)
+    {
+        if(flightArr[i] == pFlight)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int doesPlaneExist(Plane* pPlane, Plane* planeArr, size_t planeCount)
+{
+    if(planeArr == NULL)
+    {
+        return 0;
+    }
+    for(size_t i; i < planeCount; i++)
+    {
+        if(planeArr + i == pPlane)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
--- HW2/airline.h
+++ HW2/airline.h
@@ -1,16 +1,23 @@
 #ifndef __AIRLINE_H__
 #define __AIRLINE_H__
 
+#include <stdio.h>
 #include "plane.h"
 #include "flight.h"
 
 typedef struct
 {
     char* name;
+    size_t flightCount;
     Flight** flightArr;
-    int flightCount;
     Plane* planeArr;
-    int planeCount;
+    size_t planeCount;
 }Airline;
+
+int addFlight(Flight* pFlight, Airline* pAirline);  //returns 1 if the flight added successfully, 0 otherwise
+int addPlane(Plane* pPlane, Airline* pAirline);   //return 1 if the plane added successfully, 0 otherwise
+void doPrintFlightsWithPlaneType(void);
+int doesFlightExist(Flight* pFlight, Flight** flightArr, size_t flightCount);   //return 1 if flight exists, 0 otherwise
+int doesPlaneExist(Plane* pPlane, Plane* planeArr, size_t planeCount);  //returns 1 if plane exists, 0 otherwise
 
 #endif
